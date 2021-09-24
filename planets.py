6a7d055f2f16a0be9e12a9fcad6adcf4117d6cd1
8059586 <70647848+8059586@users.noreply.github.com>
2021-09-24 16:39:06 -0400

    Add files via upload

---
+++ planets.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, request
+from data import data
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return jsonify({
+        "data": data,
+        "message": "success"
+    }), 200
+
+@app.route("/planet")
+def planet():
+    for i in data:
+        planet_data = i["name"]
+    return jsonify({
+        "data": planet_data,
+        "message": "success"
+    }), 200
+
+if __name__ == "__main__":
+    app.run()
